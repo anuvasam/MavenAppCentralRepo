f3a4ca48903c9e2957a5e6e3056dd37913032575
Vinay-PC <anuvasam@gmail.com>
2020-09-30 15:48:45 +0530

    committed

---
+++ MavenApp/src/main/java/com/cg/project/sonarTest.java
@@ -0,0 +1,13 @@
+package com.cg.project;
+
+public class sonarTest {
+public static void main(String[] args) {
+	System.out.println("helloworld");
+	int i=10;
+	int s=0;
+	s+=i;
+	System.out.println("result" +s);
+	
+}
+
+}
---
+++ MavenApp/src/test/java/com/cg/projecttest/MavenProjectTest.java
@@ -0,0 +1,17 @@
+package com.cg.projecttest;
+
+import org.junit.Test;
+
+public class MavenProjectTest {
+	@Test
+	public void test1() {
+	System.out.println("Maven test");
+	}
+	@Test
+	public void test2() {
+	System.out.println("Coonection to Jenkin with GitHub ");
+	}
+
+
+}
+
