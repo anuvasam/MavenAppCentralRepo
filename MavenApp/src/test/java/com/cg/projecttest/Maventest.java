4a10637d7d83ceecfb019e56bbbd5fd45f6c997e
Vinay-PC <anuvasam@gmail.com>
2020-09-30 12:21:26 +0530

    commit the maven app

--- MavenApp/src/test/java/com/cg/projecttest/Maventest.java
+++ MavenApp/src/test/java/com/cg/projecttest/Maventest.java
@@ -1,5 +1,26 @@
 package com.cg.projecttest;
 
+import org.junit.Test;
+
 public class Maventest {
+	
+    
+	@Test
+	public void test1() {
+	System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+	System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+	System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+	System.out.println("test4");
+	}
+
 
 }
